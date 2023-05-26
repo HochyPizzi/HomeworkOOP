9857a8f140150d4d3e765778bf2f4e7857263308
Demian <kilka228378@gmail.com>
2023-05-26 18:22:48 +0300

    Выполнено домашнее задание к первому семинару

---
+++ Homework_One/src/main/java/ru/geekbrains/lesson1/Chocolate.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.lesson1;
+
+public class Chocolate extends Product{
+    private double collories;
+
+    public double getCollories() {
+        return collories;
+    }
+    public void setCollories() {
+        this.collories = collories;
+    }
+
+    public Chocolate(String brand, String name, double price, double collories) {
+        super(brand, name, price);
+        this.collories = collories;
+    }
+
+    public String displayInfo() {
+        return String.format("%s - %s - %f - %f", brand, name, price, collories);
+    }
+}
---
+++ Homework_One/src/main/java/ru/geekbrains/lesson1/VendingMachine.java
@@ -0,0 +1,35 @@
+package ru.geekbrains.lesson1;
+
+import java.util.List;
+
+public class VendingMachine {
+
+    private List<Product> products;
+
+
+    public VendingMachine(List<Product> products) {
+        this.products = products;
+    }
+
+    public BottleOfWater getBottleOfWater(double volume){
+        for (Product product : products){
+            if (product instanceof BottleOfWater){
+                if (((BottleOfWater)product).getVolume()  == volume){
+                    return (BottleOfWater)product;
+                }
+            }
+        }
+        return null;
+    }
+    public Chocolate getChocolate(double collories) {
+        for (Product product : products){
+            if (product instanceof Chocolate){
+                if (((Chocolate)product).getCollories() == collories){
+                    return  (Chocolate)product;
+                }
+            }
+        }
+        return null;
+    }
+
+}
